09b781263bb330bea8d25921e4be71dc7d8d41fc
Ogidol <ogundele115@gmail.com>
2025-08-08 10:02:24 +0100

    Implement printTeacher function and interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,7 +15,13 @@ export interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-export function printTeacher(firstName: string, lastName: string): string {
+export function printTeacher({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}): string {
   return `${firstName.charAt(0)}. ${lastName}`;
 }
 
